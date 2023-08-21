0374c3b4e79a1b4728a61152d97ea9072440cb12
Rishab Gautam <84559232+RISHAB-CREATOR@users.noreply.github.com>
2023-08-21 19:29:34 +0530

    Update Upcoming.jsx

--- src/pages/Upcoming.jsx
+++ src/pages/Upcoming.jsx
@@ -18,7 +18,7 @@ function Upcoming() {
   return (
       <>
         <Helmet>
-          <title>BlueBird Movies | Upcoming movies</title>
+          <title>RG Movies | Upcoming movies</title>
           </Helmet>
 
         <div className='w-full bg-[#10141e] md:p-10 mb-20 md:mb-0'>
