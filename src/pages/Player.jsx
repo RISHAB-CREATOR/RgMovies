51a399be85c3a5b09ddbed43f59da74d98b943b9
Rishab Gautam <84559232+RISHAB-CREATOR@users.noreply.github.com>
2023-08-22 12:21:49 +0530

    Update Player.jsx

--- src/pages/Player.jsx
+++ src/pages/Player.jsx
@@ -27,7 +27,7 @@ const Player = () => {
       }, []);
       
     
-    document.title = `BlueBird Movies | ${moviedet.title}`
+    document.title = `RG Movies | ${moviedet.title}`
 
     return (
       <>
